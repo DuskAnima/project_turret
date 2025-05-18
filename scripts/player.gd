6aa6621625f50cddd18cecc0220870614e5ba9ee
Anima <cjaccardh@gmail.com>
2025-05-18 19:48:41 -0400

    get rotated idiot + floating bullets

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,16 +9,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float):
 	mouse_follow()
-	shoot()
 
+func _input(_event: InputEvent):
+	shoot()
+	
 func mouse_follow():
 	var mouse_pos: Vector2 = get_global_mouse_position()
 	look_at(mouse_pos)
 
 func shoot():
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_just_pressed("shoot"):
+		var gun_pos = $Marker2D.get_global_position()
 		var bullet = preload("res://scenes/bullet.tscn").instantiate()
 		get_tree().current_scene.add_child(bullet)
+		bullet.set_global_position(gun_pos)
 		# infinite instantiation per frame, need to fix.
 		
 		
