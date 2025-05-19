075432ae22980780452a23c7a4cf93ee082d19f5
Anima <cjaccardh@gmail.com>
2025-05-19 16:47:09 -0400

    turretetete c o m p l e t e

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@export var speed : int = 200
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bullet_orientation()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float):
-	pass
+func _process(delta: float):
+	bullet_movement(delta)
 
 func _input(_event: InputEvent):
 	pass
@@ -17,5 +19,9 @@ func bullet_orientation():
 	look_at(mouse_pos) # Orienta el objeto hacia la posición del mouse
 	rotation_degrees += 90 # le suma 90° a la posicón en radiants para orientar el objeto hacia adelante
 
+func bullet_movement(delta):
+	position += Vector2.UP * speed * delta
+
+	
 	# Quiero que la bala se mueva hacia el mouse
 	# Necesito que detecte la posición y que luego se mueva en esa dirección
