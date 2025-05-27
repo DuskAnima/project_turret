13ac0ad3ff0f2ef6dcb2b2c0ca9d6ffcd7b2d011
Anima <cjaccardh@gmail.com>
2025-05-27 06:07:03 -0400

    enemy can die

---
+++ scripts/base_enemy.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_area_2d_area_shape_entered(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
+	queue_free()
+	pass # Replace with function body.
