a1672c1b5a79dd2300d075fd2fe0d5e107b42b2e
Anima <cjaccardh@gmail.com>
2025-05-28 06:10:32 -0400

    I may be stoopid, but it's okay, now bullets self detruct

--- scripts/base_target.gd
+++ scripts/base_target.gd
@@ -11,5 +11,5 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_area_2d_area_shape_entered(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	queue_free()
+	queue_free() #deletes the enemy
 	pass # Replace with function body.
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -25,3 +25,9 @@ func bullet_movement(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+	
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	queue_free()
