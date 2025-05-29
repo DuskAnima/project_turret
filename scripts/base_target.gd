f90dd6d1c5c4764f006e58368528f9e4f4c42e7d
Anima <cjaccardh@gmail.com>
2025-05-29 06:05:47 -0400

    target sistem

--- scripts/base_target.gd
+++ scripts/base_target.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	pass # Replace with function body.
-	print(_enabler_area_setter())
+	print(_enabler_box_area_setter())
 func _process(_delta: float) -> void:
 	pass
 
@@ -11,8 +11,8 @@ func _on_area_2d_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_in
 	if area.is_in_group("player"):
 		queue_free() #deletes the enemy
 
-# Función que establece el area del Enabler2D. 
-func _enabler_area_setter():
+# Función que establece el area del Enabler2D. ### Solo funciona si es un cuadrado! probar en otras formas
+func _enabler_box_area_setter():
 	var collision_area = $Area2D/CollisionShape2D.shape.get_rect()
 	var enabler_area = $VisibleOnScreenEnabler2D
 	enabler_area.rect = collision_area
