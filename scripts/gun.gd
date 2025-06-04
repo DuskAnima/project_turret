a3bd32b0e8fe74ccde256084c2375046ecd46149
Anima <cjaccardh@gmail.com>
2025-06-04 19:14:50 -0400

    trying to add a cooldown

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 # Varaibles de control
-var gun_enabler : bool = true # Determina si una pistola se puede disparar
+var can_shoot : bool = true # Determina si una pistola se puede disparar
+
+# Propiedades de la pistola
+var cooldown_time : float = 3.0
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Función de Input que detona un disparo con un click
 func shoot():
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and can_shoot == true:
 		_create_bullet()
 
+	
+	
+		
 # Función que instancia una bala en una posición dada
 func _create_bullet():
 	var shooter_owner = owner.get_groups()[0]
@@ -19,3 +25,8 @@ func _create_bullet():
 	get_tree().root.get_node("Main/BulletHandler").add_child(bullet)
 	bullet.global_position = global_position
 	bullet.global_rotation_degrees = global_rotation_degrees + 90
+
+
+
+func _gun_cooldown_timeout() -> void:
+	pass # Replace with function body.
