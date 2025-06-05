eb6bd8a805c4a6711aca1943ffe04cf6b147ec37
Anima <cjaccardh@gmail.com>
2025-06-05 16:03:26 -0400

    state machine process. Trying to delegate shootspeed to player node

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -3,24 +3,19 @@ extends Node2D
 # Varaibles de control
 var can_shoot : bool = true # Determina si una pistola se puede disparar
 
-# Propiedades de la pistola
-var cooldown_time : float = 3.0
-
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Función de Input que detona un disparo con un click
 func shoot():
-	if Input.is_action_just_pressed("shoot") and can_shoot == true:
-		_create_bullet()
+	if Input.is_action_just_pressed("shoot") and can_shoot == true: # Si input + arma habilitada
+		_create_bullet() # Crear bala
 
 # Función que instancia una bala en una posición dada
 func _create_bullet():
-	var shooter_owner = owner.get_groups()[0]
+	var shooter_owner : String = owner.get_groups()[0] # Accede al array de grupos y accede al string del primer grupo
 	var bullet = preload("res://scenes/bullet.tscn").instantiate() #Instanciador de balas
 	bullet.get_node("Area2D").add_to_group(shooter_owner) # Agrega el Area2D al grupo "player"
-	get_tree().root.get_node("Main/BulletHandler").add_child(bullet)
-	bullet.global_position = global_position
-	bullet.global_rotation_degrees = global_rotation_degrees + 90
-
-# Falta crear el cooldown de la pistola.
+	get_tree().root.get_node("Main/BulletHandler").add_child(bullet) # Agrega la bala a un nodo independiente en la escena
+	bullet.global_position = $Marker2D.global_position # Posiciona la bala en función al nodo Marker2D
+	bullet.global_rotation_degrees = global_rotation_degrees + 90 # Rota la bala el función a la rotación del arma/personaje
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,8 +9,10 @@ var gun : PackedScene = preload('res://scenes/gun.tscn') # Escena del arma
 
 # Propiedades del player
 var speed : int = 50 # Velocidad de movimiento
-var right_hand_gun : Node2D # Variable que almacena el arma que tendrá el player en la derecha
-var left_hand_gun : Node2D # Variable que almacena el aram que tendrá el player en la izquierda
+var right_hand_gun : Node2D # Variable que almacena instancia del arma que tendrá el player en la derecha
+var left_hand_gun : Node2D # Variable que almacena instancia del aram que tendrá el player en la izquierda
+
+
 
 func _ready():
 	right_hand_gun = _add_gun_rigth() # Asignación de la primera arma
@@ -20,27 +22,31 @@ func _process(delta: float):
 	_movement(delta)
 
 func _input(_event: InputEvent):
-	right_hand_gun.shoot()
+	right_hand_gun.shoot() # Llama al nodo de la mano izquierda con su función "disparar"
 
-# Función que define el movimiento
+# Función privada que define la velocidad del movimiento
 func _movement(delta):
-	progress += speed * delta
+	progress += speed * delta # Determina la velocidad en base al nodo principal PathFollow2D
 
-# Función que determina la dirección del Player en base a la posición del mouse
+# Función privada que determina la dirección del Player en base a la posición del mouse
 func _mouse_follow():
-	var mouse_pos: Vector2 = get_global_mouse_position()
-	look_at(mouse_pos)
+	var mouse_pos: Vector2 = get_global_mouse_position() # Obtiene las coordenadas del mouse
+	look_at(mouse_pos) # Función in-built que orienta al nodo a mirar a la dirección dada
 
-# Función para crear y manipular los nodos de armas en las manos del player
+# Funciones privadas para crear y manipular los nodos de armas en las manos del player
 func _add_gun_rigth() -> Node2D: # Nodo Gun asignado a la mano derecha
-	var right_gun_instance : Node2D = gun.instantiate()
-	$RigthHand.add_child(right_gun_instance)
-	right_hand_active = true
-	right_gun_instance.owner = self
-	return right_gun_instance
+	var right_gun_instance : Node2D = gun.instantiate() # Instanciador del arma
+	$RigthHand.add_child(right_gun_instance) # Agrega el nodo del arma a la mano derecha
+	right_hand_active = true # Habilita el uso de inputs de la mano
+	right_gun_instance.owner = self # Asigna a self la propiedad del arma (para poder asignar las balas el grupo)
+	return right_gun_instance # Retorna la instancia del arma derecha para manipularla
 func _add_gun_left() -> Node2D: # Nodo Gun asignado a la mano izquierda
-	var left_gun_instance = gun.instantiate()
-	$LeftHand.add_child(left_gun_instance)
-	left_hand_active = true
-	left_gun_instance.owner = self
-	return left_gun_instance
+	var left_gun_instance = gun.instantiate() # Instanciador del arma
+	$LeftHand.add_child(left_gun_instance) # Agrega el nodo del arma a la mano izquierda
+	left_hand_active = true # Habilita el uso de inputs de la mano
+	left_gun_instance.owner = self # Asigna a self la propiedad del arma (para poder asignar las balas el grupo)
+	return left_gun_instance # Retorna la instancia del arma izquierda para manipularla
+
+
+func _on_shoot_cooldown_timeout() -> void:
+	pass # Replace with function body.
